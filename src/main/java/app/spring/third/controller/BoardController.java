41615e2d49cccac5c5d51a6ae779a85fb6bcef0e
KANE350 <pinpin3573@gmail.com>
2022-06-21 16:59:01 +0900

    membercommit2

--- src/main/java/app/spring/third/controller/BoardController.java
+++ src/main/java/app/spring/third/controller/BoardController.java
@@ -1,8 +1,10 @@
 package app.spring.third.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("board")
 public class BoardController {
 	
 }
--- src/main/java/app/spring/third/controller/ReplyController.java
+++ src/main/java/app/spring/third/controller/ReplyController.java
@@ -1,8 +1,10 @@
 package app.spring.third.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("reply")
 public class ReplyController {
 
 }
